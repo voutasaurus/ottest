2a946a738e212c2b70a37425896af81369b7b215
Anthony Voutas <anthonyv@kiva.org>
2019-09-05 13:17:19 -0700

    add circleci test

---
+++ test.sh
@@ -0,0 +1,6 @@
+curl http://localhost:8080/hello
+curl http://localhost:8081/hello
+curl http://localhost:8082/hello
+curl http://localhost:8080/
+curl http://localhost:8081/
+curl http://localhost:8082/
