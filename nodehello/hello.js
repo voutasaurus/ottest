6129cc29a56109db58b3de423766cd2ba1956f3a
Anthony Voutas <anthonyv@kiva.org>
2019-09-06 11:17:04 -0700

    add node client endpoint

--- nodehello/hello.js
+++ nodehello/hello.js
@@ -5,12 +5,18 @@ const middleware = et.default;
 const { initTracer } = require("./tracing");
 const tracer = initTracer("node-hello");
 const port = 3000;
+const http = require('axios');
 
 app.use(middleware({tracer: tracer}));
 
-app.get('/', (req, res) => {
-    // TODO: hit other services
-  res.send('Hello Node!')
+app.get('/', async (req, res) => {
+  var svc = process.env.SERVICES.split(',');
+  var rr = [];
+  for (const url of svc) {
+    var response = await http.get(url);
+    rr.push(response.data);
+  };
+  res.send("NestJS root, other services: " + rr.toString());
 })
 
 app.get('/hello', (req, res) => {
