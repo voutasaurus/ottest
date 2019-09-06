726db019777cb2a4b296500ca475be7ac71293bf
Anthony Voutas <anthonyv@kiva.org>
2019-09-06 09:11:44 -0700

    fix nest client

--- nesthello/src/app.controller.ts
+++ nesthello/src/app.controller.ts
@@ -1,23 +1,15 @@
-import { Controller, Get, HttpService } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
     constructor(
         private readonly appService: AppService,
-        // private readonly http: HttpService,
-    ) {}
+    ) {};
 
   @Get()
-  getRoot(): string {
-      var svc = process.env.SERVICES.split(',');
-      var res = [];
-      svc.forEach(function(url, index) {
-          this.http.get(url).then(function (response) {
-              res.push(response.data);
-          });
-      });
-      return "NestJS root, other services: " + res.toString();
+  async getRoot(): Promise<string> {
+    return await this.appService.getRoot();
   }
 
   @Get('hello')
--- nesthello/src/app.service.ts
+++ nesthello/src/app.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpService } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  constructor(
+    private readonly http: HttpService,
+  ) {};
+
   getHello(): string {
     return 'Hello Nest!';
+  }
+
+  async getRoot(): Promise<string> {
+    var svc = process.env.SERVICES.split(',');
+    var res = [];
+    var self = this;
+    for (const url of svc) {
+      var response = await self.http.get(url).toPromise();
+      res.push(response.data);
+    };
+    return "NestJS root, other services: " + res.toString();
   }
 }
